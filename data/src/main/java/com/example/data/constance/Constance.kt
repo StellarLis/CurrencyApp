2b88cd329aa63b69e67823d808dcde6378713679
Andrew <86295320+StellarLis@users.noreply.github.com>
2023-01-28 12:57:04 +0300

    Commit

--- data/src/main/java/com/example/data/constance/Constance.kt
+++ data/src/main/java/com/example/data/constance/Constance.kt
@@ -1,6 +1,6 @@
 package com.example.data.constance
 
 object Constance {
-    const val API_KEY = "TghdV9FIKVVRZmVL1pneR8cCDD9B6ASD" // your API key here
+    const val API_KEY = "" // your API key here
     const val BASE_URL = "https://api.apilayer.com/exchangerates_data/"
 }
